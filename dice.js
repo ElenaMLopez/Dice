2caea503ae8a37d105272e5e3940207ae7bb26be
elenamlopez <elena.mateos1@gmail.com>
2018-05-12 21:50:30 +0200

    Added dice and styles

--- dice.js
+++ dice.js
@@ -1,4 +1,17 @@
-function tiraDado(){
-  var dado1 =  Math.floor(Math.random()*10)+1; // como patron entraría tb en una funcion
-  var dado2 =  Math.floor(Math.random()*10)+1;
-    }
+
+var rollButton = document.getElementsByClassName('player-button'),
+    dice1 = document.getElementById('dice1'),
+    dice2 = document.getElementById('dice2');
+
+function hide(element){
+  element.classList.toggle('hidden');
+}
+for (var i = 0; i < rollButton.length; i++) {
+  rollButton[i].addEventListener('click', function(){
+    dice1.value =  Math.floor(Math.random()*10)+1; // como patron entraría tb en una funcion
+    dice2.value =  Math.floor(Math.random()*10)+1;
+    console.log('dado1: '+ dice1.value);
+    console.log('dado2: '+ dice2.value);
+    hide(this);
+  });
+}
